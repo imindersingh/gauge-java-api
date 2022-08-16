2346b2b6399d20f2da17d9a1f0da5b3648600c3d
iminder.rehal <iminder.rehal@bottomline.com>
2022-08-16 11:36:49 +0100

    Updating class generics and adding github workflows

--- src/test/java/com/imindersingh/http/Requests.java
+++ src/test/java/com/imindersingh/http/Requests.java
@@ -33,21 +33,21 @@ public abstract class Requests {
         this.host = host;
     }
 
-    public HttpResponse getRequest(String path, Map<String, String> headers) {
+    public HttpResponse<?> getRequest(String path, Map<String, String> headers) {
         return this.getRequest(path, headers, Type.JSON);
     }
 
-    public HttpResponse getRequestAsString(String path, Map<String, String> headers) {
+    public HttpResponse<?> getRequestAsString(String path, Map<String, String> headers) {
         return this.getRequest(path, headers, Type.STRING);
     }
 
-    public HttpResponse getRequest(String path, Map<String, String> headers, Type type) {
-        HttpRequest request = Unirest.get(this.host + path).headers(headers);
+    public HttpResponse<?> getRequest(String path, Map<String, String> headers, Type type) {
+        HttpRequest<?> request = Unirest.get(this.host + path).headers(headers);
         return this.send(request, type);
     }
 
-    private HttpResponse send(HttpRequest request, Type type) {
-        HttpResponse response = null;
+    private HttpResponse<?> send(HttpRequest<?> request, Type type) {
+        HttpResponse<?> response = null;
         try {
             if (type == Type.JSON) {
                 response = request.asJson();
@@ -61,91 +61,91 @@ public abstract class Requests {
         }
     }
 
-    private HttpResponse bodyRequest(Requests.RequestType requestType, String path, Map<String, String> headers, String body, Type type) {
-        HttpRequest request = ((HttpRequestWithBody)requestType.method(this.host + path).headers(headers)).body(body);
+    private HttpResponse<?> bodyRequest(Requests.RequestType requestType, String path, Map<String, String> headers, String body, Type type) {
+        HttpRequest<?> request = ((HttpRequestWithBody)requestType.method(this.host + path).headers(headers)).body(body);
         return this.send(request, type);
     }
 
-    private HttpResponse fieldsRequest(Requests.RequestType requestType, String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
+    private HttpResponse<?> fieldsRequest(Requests.RequestType requestType, String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
         MultipartBody request = ((HttpRequestWithBody)requestType.method(this.host + path).headers(headers)).fields(fields);
         return this.send(request, type);
     }
 
-    private HttpResponse optionsRequest(String path, Map<String, String> headers, Type type) {
+    private HttpResponse<?> optionsRequest(String path, Map<String, String> headers, Type type) {
         GetRequest request = (GetRequest)Unirest.options(this.host + path).headers(headers);
         return this.send(request, type);
     }
 
-    private HttpResponse noBodyRequest(Requests.RequestType requestType, String path, Map<String, String> headers, Type type) {
-        HttpRequest request = requestType.method(this.host + path).headers(headers);
+    private HttpResponse<?> noBodyRequest(Requests.RequestType requestType, String path, Map<String, String> headers, Type type) {
+        HttpRequest<?> request = requestType.method(this.host + path).headers(headers);
         return this.send(request, type);
     }
 
-    public HttpResponse deleteRequest(String path, Map<String, String> headers, String body, Type type) {
+    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, String body, Type type) {
         return this.bodyRequest(Unirest::delete, path, headers, body, type);
     }
 
-    public HttpResponse deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
+    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
         return this.fieldsRequest(Unirest::delete, path, headers, fields, type);
     }
 
-    public HttpResponse deleteRequest(String path, Map<String, String> headers, String body) {
+    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, String body) {
         return this.deleteRequest(path, headers, body, Type.JSON);
     }
 
-    public HttpResponse deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
+    public HttpResponse<?> deleteRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
         return this.deleteRequest(path, headers, fields, Type.JSON);
     }
 
-    protected HttpResponse deleteRequest(String path, Map<String, String> headers) {
+    protected HttpResponse<?> deleteRequest(String path, Map<String, String> headers) {
         return this.noBodyRequest(Unirest::delete, path, headers, Type.JSON);
     }
 
-    protected HttpResponse postRequest(String path, Map<String, String> headers) {
+    protected HttpResponse<?> postRequest(String path, Map<String, String> headers) {
         return this.noBodyRequest(Unirest::post, path, headers, Type.JSON);
     }
 
-    protected HttpResponse postRequest(String path, Map<String, String> headers, Type type) {
+    protected HttpResponse<?> postRequest(String path, Map<String, String> headers, Type type) {
         return this.noBodyRequest(Unirest::post, path, headers, type);
     }
 
-    public HttpResponse postRequest(String path, Map<String, String> headers, String body, Type type) {
+    public HttpResponse<?> postRequest(String path, Map<String, String> headers, String body, Type type) {
         return this.bodyRequest(Unirest::post, path, headers, body, type);
     }
 
-    public HttpResponse postRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
+    public HttpResponse<?> postRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
         return this.fieldsRequest(Unirest::post, path, headers, fields, type);
     }
 
-    public HttpResponse postRequest(String path, Map<String, String> headers, String body) {
+    public HttpResponse<?> postRequest(String path, Map<String, String> headers, String body) {
         return this.postRequest(path, headers, body, Type.JSON);
     }
 
-    public HttpResponse postRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
+    public HttpResponse<?> postRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
         return this.postRequest(path, headers, fields, Type.JSON);
     }
 
-    public HttpResponse putRequest(String path, Map<String, String> headers) {
+    public HttpResponse<?> putRequest(String path, Map<String, String> headers) {
         return this.noBodyRequest(Unirest::put, path, headers, Type.JSON);
     }
 
-    public HttpResponse putRequest(String path, Map<String, String> headers, String body, Type type) {
+    public HttpResponse<?> putRequest(String path, Map<String, String> headers, String body, Type type) {
         return this.bodyRequest(Unirest::put, path, headers, body, type);
     }
 
-    public HttpResponse putRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
+    public HttpResponse<?> putRequest(String path, Map<String, String> headers, Map<String, Object> fields, Type type) {
         return this.fieldsRequest(Unirest::put, path, headers, fields, type);
     }
 
-    public HttpResponse putRequest(String path, Map<String, String> headers, String body) {
+    public HttpResponse<?> putRequest(String path, Map<String, String> headers, String body) {
         return this.putRequest(path, headers, body, Type.JSON);
     }
 
-    public HttpResponse putRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
+    public HttpResponse<?> putRequest(String path, Map<String, String> headers, Map<String, Object> fields) {
         return this.putRequest(path, headers, fields, Type.JSON);
     }
 
-    protected HttpResponse optionsRequest(String path, Map<String, String> headers) {
+    protected HttpResponse<?> optionsRequest(String path, Map<String, String> headers) {
         return this.optionsRequest(path, headers, Type.JSON);
     }
 
--- src/test/java/com/imindersingh/steps/ReferenceApiSteps.java
+++ src/test/java/com/imindersingh/steps/ReferenceApiSteps.java
@@ -13,12 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReferenceApiSteps {
 
-    private DataStore dataStore = new DataStore();
-    private ReferenceApiRequests request = new ReferenceApiRequests();
+    private final DataStore dataStore = new DataStore();
+    private final ReferenceApiRequests request = new ReferenceApiRequests();
 
     @Step("GET request to <endpoint> endpoint")
     public void getRequest(final String endpoint) {
-        HttpResponse response = request.get(endpoint);
+        HttpResponse<?> response = request.get(endpoint);
         dataStore.put("response", response);
     }
 
@@ -28,7 +28,7 @@ public class ReferenceApiSteps {
         postBody.setTest(body);
         String requestBody = new Gson().toJson(postBody);
         dataStore.put("body", postBody.getTest());
-        HttpResponse response = request.post(endpoint, requestBody);
+        HttpResponse<?> response = request.post(endpoint, requestBody);
         dataStore.put("response", response);
     }
 
@@ -37,29 +37,29 @@ public class ReferenceApiSteps {
         PostBody postBody = new PostBody();
         postBody.setTest("test1234");
         String body = new Gson().toJson(postBody);
-        HttpResponse response = request.post(endpoint, body);
+        HttpResponse<?> response = request.post(endpoint, body);
         dataStore.put("response", response);
     }
 
     @Step("Assert the http response code is <httpResponseCode>")
     public void assertHttpResponseCode(final String httpResponseCode) {
-        HttpResponse response = (HttpResponse) dataStore.get("response");
+        HttpResponse<?> response = (HttpResponse<?>) dataStore.get("response");
         assertThat(response.getStatus()).isEqualTo(Integer.parseInt(httpResponseCode));
     }
 
     @Step("Assert the http response text is <httpResponseText>")
     public void assertHttpResponseText(final String httpResponseText) {
-        HttpResponse response = (HttpResponse) dataStore.get("response");
+        HttpResponse<?> response = (HttpResponse<?>) dataStore.get("response");
         assertThat(response.getStatusText()).isEqualTo(httpResponseText);
     }
 
     @Step("Assert the http response body contains the posted value")
     public void assertHttpResponseBody() {
-        HttpResponse response = (HttpResponse) dataStore.get("response");
+        HttpResponse<?> response = (HttpResponse<?>) dataStore.get("response");
         JSONObject jsonObject = new JSONObject(response.getBody().toString());
         String responseBody = jsonObject.getJSONArray("receivedRequest").getJSONObject(0).get("test").toString();
 //        Gson gson = new Gson();
 //        User user = gson.fromJson(json_string, User.class);
-        assertThat(responseBody).isEqualTo((String) dataStore.get("body"));
+        assertThat(responseBody).isEqualTo(dataStore.get("body"));
     }
 }
